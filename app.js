477e200a03154e6bae4932d6058343446b578c68
Tommy0901 <ak710180@gmail.com>
2023-12-08 17:06:56 +0800

    install package (using npm audit fix)

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(routes)
 
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
+  console.log(`Express server is running on http://localhost:${port}`)
 })
 
 module.exports = app
