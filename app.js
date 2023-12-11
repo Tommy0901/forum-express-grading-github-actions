ba134d620a789f32095911ad92103fe4cb016d68
Tommy0901 <ak710180@gmail.com>
2023-12-11 14:17:55 +0800

    feat: add header & footer

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const { engine: handlebars } = require('express-handlebars')
 const flash = require('connect-flash')
 const passport = require('./config/passport')
 const routes = require('./routes')
+const helpers = require('./helpers/handlebars-helpers.js')
+const { getUser } = require('./helpers/auth-helpers.js')
 
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
 
-app.engine('.hbs', handlebars({ extname: '.hbs' })) // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
+app.engine('.hbs', handlebars({ extname: '.hbs', helpers })) // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
 app.set('view engine', '.hbs') // 設定使用 Handlebars 做為樣板引擎
 app.set('views', './views') // 參考之前作法加入此行程式碼
 
@@ -21,6 +23,7 @@ app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success')
   res.locals.error_msg = req.flash('error')
+  res.locals.user = getUser(req)
   next()
 })
 app.use(routes)
---
+++ helpers/auth-helpers.js
@@ -0,0 +1,3 @@
+module.exports = {
+  getUser (req) { return req.user || null }
+}
