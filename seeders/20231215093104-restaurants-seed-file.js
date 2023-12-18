bfb1f9af76a6a78f70f499d9eb6c6abc8898a909
Tommy0901 <ak710180@gmail.com>
2023-12-18 13:26:12 +0800

    alter seed for Github actions : (((

--- seeders/20231215093104-restaurants-seed-file.js
+++ seeders/20231215093104-restaurants-seed-file.js
@@ -1,39 +1,21 @@
 'use strict'
 const faker = require('faker')
-const fs = require('fs')
-
-const { downloadFileHandler } = require('../helpers/download-helpers')
-
-const { Restaurant } = require('../models')
-const { Op } = require('sequelize')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const counts = await Restaurant.count()
-    const downloadpath = `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`
-    await Promise.all(Array.from({ length: 50 }).map((_, i) => (downloadFileHandler(downloadpath, i + 1 + counts))))
     await queryInterface.bulkInsert('Restaurants',
-      Array.from({ length: 50 }).map((_, i) => ({
+      Array.from({ length: 50 }, () => ({
         name: faker.name.findName(),
         tel: faker.phone.phoneNumber(),
         address: faker.address.streetAddress(),
         opening_hours: '08:00',
-        image: `/upload/${i + 1 + counts}.jpg`,
+        image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
         description: faker.lorem.text()
       }))
     )
   },
   down: async (queryInterface, Sequelize) => {
-    const counts = await Restaurant.count()
-    const { id } = await Restaurant.findOne({ order: [['id', 'DESC']], raw: true })
-    for (let i = 0; i < 50; i++) {
-      fs.unlink(`public/upload/${counts - i}.jpg`, error =>
-        error
-          ? console.error('Error deleting file:', error)
-          : console.log(`Image upload/${counts}.jpg was deleted`)
-      )
-    }
-    await queryInterface.bulkDelete('Restaurants', { id: { [Op.gt]: id - 50 } })
+    await queryInterface.bulkDelete('Restaurants', null, {})
   }
 }
