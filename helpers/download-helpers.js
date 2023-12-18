b27ff2588995b088bc4fec1acd248b204e9b4498
Tommy0901 <ak710180@gmail.com>
2023-12-18 12:55:47 +0800

    optimize using seed

---
+++ helpers/download-helpers.js
@@ -0,0 +1,18 @@
+const fs = require('fs') // 引入 fs 模組
+const axios = require('axios')
+
+module.exports = {
+  downloadFileHandler (downloadpath, fileNumber) { // file 是 multer 處理完的檔案
+    return new Promise((resolve, reject) => {
+      (async () => {
+        try {
+          const response = await axios.get(downloadpath, { responseType: 'arraybuffer' })
+          fs.writeFileSync(`public/upload/${fileNumber}.jpg`, Buffer.from(response.data))
+          resolve(console.log(`Image downloaded and saved to upload/${fileNumber}.jpg`))
+        } catch (eroror) {
+          reject(eroror)
+        }
+      })()
+    })
+  }
+}
