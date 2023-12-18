e701b29b17acf6254dc59c97b6131cea37620d6e
Tommy0901 <ak710180@gmail.com>
2023-12-19 00:27:29 +0800

    feat: add ifCond hbs helper & update category selector

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -2,5 +2,5 @@ const dayjs = require('dayjs') // 載入 dayjs 套件
 
 module.exports = {
   currentYear () { return dayjs().year() }, // 取得當年年份作為 currentYear 的屬性值，並導出
-  ifCond (a, b, options) { return a === b ? options.fn(this) : options.inverse(this) }
+  ifCond (a, b, options) { return a === b ? options.fn(this) : options.inverse(this) } // 可參考 HandlebarS 官網的 Block Helpers Conditonals；注意使用 this 不要搭配箭頭函式
 }
