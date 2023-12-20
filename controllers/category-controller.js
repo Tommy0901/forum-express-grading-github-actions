70757a749a04b9edff8a27d57f48a4f52279e9a3
Tommy0901 <ak710180@gmail.com>
2023-12-20 14:39:15 +0800

    feat: add admin categories page

---
+++ controllers/category-controller.js
@@ -0,0 +1,59 @@
+const { Category } = require('../models')
+
+const categoryController = {
+  getCategories: (req, res, next) => {
+    const id = req.params?.id;
+    (async () => {
+      try {
+        const [categories, category] = await Promise.all([Category.findAll({ raw: true }), Category.findByPk(id, { raw: true })])
+        res.render('admin/categories', { categories, category })
+      } catch (error) {
+        next(error)
+      }
+    })()
+  },
+  postCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category needs name to add.');
+    (async () => {
+      try {
+        await Category.create({ name })
+        req.flash('success', 'category was successfully added.')
+        res.redirect('/admin/categories')
+      } catch (error) {
+        next(error)
+      }
+    })()
+  },
+  putCategory: (req, res, next) => {
+    const { id } = req.params
+    const { name } = req.body
+    if (!name) throw new Error('Category needs name to add.');
+    (async () => {
+      try {
+        const category = await Category.findByPk(id) // 接著操作 Sequelize 語法，不加 { raw: true }
+        if (!category) throw new Error("Category didn't exist!")
+        await category.update({ name })
+        req.flash('success', 'category was successfully updated!')
+        res.redirect('/admin/categories')
+      } catch (error) {
+        next(error)
+      }
+    })()
+  },
+  deleteCategory: (req, res, next) => {
+    const { id } = req.params;
+    (async () => {
+      try {
+        const category = await Category.findByPk(id) // 接著操作 Sequelize 語法，不加 { raw: true }
+        if (!category) throw new Error("Category didn't exist!")
+        await category.destroy()
+        req.flash('success', 'category was successfully deleted!')
+        res.redirect('/admin/categories')
+      } catch (error) {
+        next(error)
+      }
+    })()
+  }
+}
+module.exports = categoryController
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,11 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const adminController = require('../../controllers/admin-controller')
-const { upload } = require('../../middlewares/multer')
+const categoryController = require('../../controllers/category-controller')
 
-router.get('/users/new', adminController.registerUser)
-router.patch('/users/:id', adminController.patchUser)
-router.get('/users', adminController.getUsers)
+const { upload } = require('../../middlewares/multer')
 
 router.get('/restaurants/create', adminController.createRestaurant)
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
@@ -15,6 +13,16 @@ router.put('/restaurants/:id', upload.single('image'), adminController.putRestau
 router.get('/restaurants/:id', adminController.getRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
+
+router.get('/users/new', adminController.registerUser)
+router.patch('/users/:id', adminController.patchUser)
+router.get('/users', adminController.getUsers)
+
+router.delete('/categories/:id', categoryController.deleteCategory)
+router.put('/categories/:id', categoryController.putCategory)
+router.get('/categories/:id', categoryController.getCategories)
+router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
 
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
