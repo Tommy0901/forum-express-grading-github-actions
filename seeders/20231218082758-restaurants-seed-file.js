7cb3ca5224338f9fad7dea8e2599517a70b7b146
Tommy0901 <ak710180@gmail.com>
2024-01-02 18:27:24 +0800

    modify seed data for github actions testing

--- seeders/20231218082758-restaurants-seed-file.js
+++ seeders/20231218082758-restaurants-seed-file.js
@@ -2,7 +2,7 @@
 const faker = require('faker')
 const fs = require('fs')
 
-const { downloadImageHandler } = require('../helpers/download-helpers')
+// const { downloadImageHandler } = require('../helpers/download-helpers')
 
 const { Restaurant } = require('../models')
 const { Op } = require('sequelize')
@@ -10,20 +10,20 @@ const { Op } = require('sequelize')
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const counts = await Restaurant.count()
+    // const counts = await Restaurant.count()
     const categories = await queryInterface.sequelize.query(
       'SELECT id FROM Categories;',
       { type: queryInterface.sequelize.QueryTypes.SELECT }
     )
-    const downloadpath = `https://loremflickr.com/320/240/food?/random=${Math.random() * 100}`
-    await Promise.all(Array.from({ length: 50 }).map((_, i) => (downloadImageHandler(downloadpath, i + 1 + counts))))
+    // const downloadpath = `https://loremflickr.com/320/240/food?/random=${Math.random() * 100}`
+    // await Promise.all(Array.from({ length: 50 }).map((_, i) => (downloadImageHandler(downloadpath, i + 1 + counts))))
     await queryInterface.bulkInsert('Restaurants',
       Array.from({ length: 50 }).map((_, i) => ({
         name: faker.name.findName(),
         tel: faker.phone.phoneNumber(),
         address: faker.address.streetAddress(),
         opening_hours: '08:00',
-        image: `/upload/${i + 1 + counts}.jpg`,
+        image: `https://loremflickr.com/320/240/food/?random=${Math.random() * 100}`,
         description: faker.lorem.text(),
         category_id: categories[Math.floor(Math.random() * categories.length)].id
       }))
