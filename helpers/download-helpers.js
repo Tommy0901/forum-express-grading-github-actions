e6a1ce9ddf0f6de089f1caf164ebc14232cafc71
Tommy0901 <ak710180@gmail.com>
2024-01-09 00:48:17 +0800

    feat: add imgur api & get response URL handler

--- helpers/download-helpers.js
+++ helpers/download-helpers.js
@@ -9,8 +9,21 @@ module.exports = {
           const response = await axios.get(downloadpath, { responseType: 'arraybuffer' })
           fs.writeFileSync(`public/upload/${fileNumber}.jpg`, Buffer.from(response.data))
           resolve(console.log(`Image downloaded and saved to upload/${fileNumber}.jpg`))
-        } catch (eroror) {
-          reject(eroror)
+        } catch (err) {
+          reject(err)
+        }
+      })()
+    })
+  },
+  responseUrlHandler (url) { // file 是 multer 處理完的檔案
+    return new Promise((resolve, reject) => {
+      (async () => {
+        try {
+          const response = await axios.get(url, { responseType: 'arraybuffer' })
+          const IncomingMessage = response.request.res
+          resolve(IncomingMessage?.responseUrl || null)
+        } catch (err) {
+          reject(err)
         }
       })()
     })
--- helpers/file-helpers.js
+++ helpers/file-helpers.js
@@ -1,4 +1,5 @@
 const fs = require('fs') // 引入 fs 模組
+const imgur = require('imgur')
 
 module.exports = {
   localFileHandler (file) { // file 是 multer 處理完的檔案
@@ -8,8 +9,21 @@ module.exports = {
         try {
           await fs.promises.writeFile(`public/upload/${file.originalname}`, await fs.promises.readFile(file.path))
           resolve(`/upload/${file.originalname}`)
-        } catch (eroror) {
-          reject(eroror)
+        } catch (err) {
+          reject(err)
+        }
+      })()
+    })
+  },
+  imgurFileHandler (file) { // file 是 multer 處理完的檔案
+    return new Promise((resolve, reject) => {
+      if (!file) return resolve(null);
+      (async () => {
+        try {
+          const img = await imgur.uploadFile(file.path)
+          resolve(img?.link || null)
+        } catch (err) {
+          reject(err)
         }
       })()
     })
