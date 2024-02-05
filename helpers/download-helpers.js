e21cd488a138e9bdb29fbaf8fa0f7ecbb081f7fd
Tommy0901 <ak710180@gmail.com>
2024-02-05 20:48:55 +0800

    feat: install @google-cloud/storage

--- helpers/download-helpers.js
+++ helpers/download-helpers.js
@@ -1,5 +1,13 @@
 const fs = require('fs') // 引入 fs 模組
 const axios = require('axios')
+const { Storage } = require('@google-cloud/storage')
+
+const storage = new Storage({
+  projectId: 'silver-fragment-411909',
+  keyFilename: '/home/welcome-rsa-key-20240205/download/silver-fragment-411909-86d979044673.json' // 替換成你的服務帳戶金鑰路徑
+})
+
+const bucketName = 'test_upload_image' // 替換成你的 GCS 存儲桶名稱
 
 module.exports = {
   downloadImageHandler (downloadpath, fileNumber) { // file 是 multer 處理完的檔案
@@ -15,7 +23,7 @@ module.exports = {
       })()
     })
   },
-  responseUrlHandler (url) { // file 是 multer 處理完的檔案
+  responseUrlHandler (url) {
     return new Promise((resolve, reject) => {
       (async () => {
         try {
@@ -27,5 +35,51 @@ module.exports = {
         }
       })()
     })
+  },
+  uploadImageFromUrlToGCS (downloadpath, fileNumber) {
+    return new Promise((resolve, reject) => {
+      (async () => {
+        try {
+          // 下載圖片
+          const response = await axios.get(downloadpath, { responseType: 'arraybuffer' })
+          // 上傳到 GCS
+          await uploadToGCS(Buffer.from(response.data), `${fileNumber}.jpg`)
+          resolve(console.log('Image uploaded to GCS'))
+        } catch (err) {
+          reject(err)
+        }
+      })()
+    })
+  },
+  deleteFileInGCS (fileNumber) {
+    return new Promise((resolve, reject) => {
+      (async () => {
+        try {
+          await deleteFile(fileNumber)
+          resolve(console.log('File deleted in GCS'))
+        } catch (err) {
+          reject(err)
+        }
+      })()
+    })
+  }
+}
+
+async function uploadToGCS (fileBuffer, remoteFilePath) {
+  try {
+    // 上傳到 GCS
+    await storage.bucket(bucketName).file(remoteFilePath).save(fileBuffer)
+    console.log(`File uploaded to GCS: ${remoteFilePath}`)
+  } catch (error) {
+    console.error(`Error uploading file: ${error.message}`)
+  }
+}
+
+async function deleteFile (fileNumber) {
+  try {
+    await storage.bucket(bucketName).file(`${fileNumber}.jpg`).delete()
+    console.log(`File ${fileNumber}.jpg deleted successfully.`)
+  } catch (error) {
+    console.error(`Error deleting file: ${error.message}`)
   }
 }
--- seeders/20231218082758-restaurants-seed-file.js
+++ seeders/20231218082758-restaurants-seed-file.js
@@ -2,8 +2,7 @@
 const faker = require('faker')
 // const fs = require('fs')
 
-const { responseUrlHandler } = require('../helpers/download-helpers')
-// const { downloadImageHandler } = require('../helpers/download-helpers')
+const { uploadImageFromUrlToGCS, deleteFileInGCS } = require('../helpers/download-helpers')
 
 const { Restaurant } = require('../models')
 const { Op } = require('sequelize')
@@ -11,38 +10,31 @@ const { Op } = require('sequelize')
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    // const counts = await Restaurant.count()
+    const counts = await Restaurant.count()
     const categories = await queryInterface.sequelize.query(
       'SELECT id FROM Categories;',
       { type: queryInterface.sequelize.QueryTypes.SELECT }
     )
-    const url = `https://loremflickr.com/320/240/food?/random=${Math.random() * 100}`
-    const responseUrlArr = await Promise.all(Array.from({ length: 50 }).map(() => (responseUrlHandler(url))))
-    // const downloadpath = `https://loremflickr.com/320/240/food?/random=${Math.random() * 100}`
-    // await Promise.all(Array.from({ length: 50 }).map((_, i) => (downloadImageHandler(downloadpath, i + 1 + counts))))
+    const downloadpath = `https://loremflickr.com/320/240/food?/random=${Math.random() * 100}`
+    await Promise.all(Array.from({ length: 50 }).map((_, i) => (uploadImageFromUrlToGCS(downloadpath, i + 1 + counts))))
     await queryInterface.bulkInsert('Restaurants',
       Array.from({ length: 50 }).map((_, i) => ({
         name: faker.name.findName(),
         tel: faker.phone.phoneNumber(),
         address: faker.address.streetAddress(),
         opening_hours: '08:00',
-        image: responseUrlArr[i],
-        // image: `/upload/${i + 1 + counts}.jpg`,
+        image: `https://storage.googleapis.com/test_upload_image/${i + 1 + counts}.jpg`,
         description: faker.lorem.text(),
         category_id: categories[Math.floor(Math.random() * categories.length)].id
       }))
     )
   },
   down: async (queryInterface, Sequelize) => {
-    // const counts = await Restaurant.count()
+    const counts = await Restaurant.count()
     const { id } = await Restaurant.findOne({ order: [['id', 'DESC']], raw: true })
-    // for (let i = 0; i < 50; i++) {
-    //   fs.unlink(`public/upload/${counts - i}.jpg`, error =>
-    //     error
-    //       ? console.error('Error deleting file:', error)
-    //       : console.log(`Image upload/${counts - i}.jpg was deleted`)
-    //   )
-    // }
+    for (let i = 0; i < 50; i++) {
+      deleteFileInGCS(counts - i)
+    }
     await queryInterface.bulkDelete('Restaurants', { id: { [Op.gt]: id - 50 } })
   }
 }
